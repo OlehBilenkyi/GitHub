a3b4bcc09e0edc9cfe1045cf80be1081d6b39ac7
Oleh Belenkuy <I3eLuy@gmail.com>
2024-09-03 12:34:22 +0300

    1

---
+++ Test1/Project/script.js
@@ -0,0 +1,10 @@
+function contains(arr, num) {
+    return arr.includes(num);
+}
+
+// Примеры использования функции
+const numbers = [1, 2, 3, 4, 5];
+
+const a = [1,2,3,'g','d']
+
+console.log(contains(a,3))
