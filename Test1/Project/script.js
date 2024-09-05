5ae272b7a67c7e583e245c1c30afbd639d77de2c
Oleh Belenkuy <I3eLuy@gmail.com>
2024-09-05 18:38:24 +0300

    ...

--- Test1/Project/script.js
+++ Test1/Project/script.js
@@ -1,8 +1,8 @@
 //let arr = [1, 2, 3, 4, 5, 6,8,10,11];
 
-function zalupa(str) {
+function zalupa(num) {
   
-  return str.toString()
+  return num.toString().split("").reduce((sum, digit)=> sum + Number(digit), 0)
     
 }
 console.log(zalupa(123));
