3c2864d11a0e7cc658f0c9ea57c163c2d122748e
Oleh Belenkuy <I3eLuy@gmail.com>
2024-09-06 18:14:38 +0300

    0

--- Test1/Project/script.js
+++ Test1/Project/script.js
@@ -1,19 +1,14 @@
-let myArray = [NaN];
+let myArray = [1,2,3];
+let myArray2 = ['qwe','zxc']
 
-function secondLargest(arr) {
-  let maxNumber = -Infinity;
-  let secondMaxNumber = -Infinity;
-
-  for (let value of arr) {
-    if (value > maxNumber) {
-      secondMaxNumber = maxNumber; 
-      maxNumber = value;
-    } else if (value > secondMaxNumber && value < maxNumber) { 
-      secondMaxNumber = value;
-    }
-  }
- return secondMaxNumber; 
+function secondLargest(arr1,arr2) {
+  return  [
+      ...arr1, 
+      ...arr2
+      
+    ]
+  
 }
-console.log(secondLargest(myArray));
+console.log(secondLargest(myArray,myArray2));
 
 
