e5248561af2118bc72d4277c35897ed1fde7c10f
Oleh Belenkuy <I3eLuy@gmail.com>
2024-09-07 13:45:05 +0300

    i++j++ Преобразование массива объектов в объект Описание: Напишите функцию, которая принимает массив объектов с ключами id и возвращает объект, где ключи — это id, а значения — это соответствующие объекты.

--- Test1/Project/script.js
+++ Test1/Project/script.js
@@ -1,13 +1,14 @@
-myArray = [
-  {name: 'Oli', age: 25},
-  {name: 'Oleg', age: 30},
-  {name: 'Alex', age: 25}
-]
+const myArray = [
+  {id:1, name: 'John'},
+  {id:2, name: 'Jane'},
+  {id:3, name: 'Jim'},
+ ]
 
-function vozrast(arr,num){
-  
-  const newArray = arr.filter((el)  => el.age == num) 
-  return newArray
+ function change(arr) {
+  let newObject = {};
+    for(let i = 0, j = 1; i < arr.length; i++, j++){
+     newObject[j] = arr[i]
 }
-
-console.log(vozrast(myArray,30))
+return newObject
+ }
+ console.log(change(myArray))
