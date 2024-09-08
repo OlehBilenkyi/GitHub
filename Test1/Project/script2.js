5d60de38ba9117b20058dcc43a1cefa9797b80f2
Serhiy7 <ssukalo79@gmail.com>
2024-09-08 16:13:13 +0200

    findKeyByValue

--- Test1/Project/script2.js
+++ Test1/Project/script2.js
@@ -1,31 +1,13 @@
-//Напишите функцию secondLargest(arr), которая принимает массив чисел и возвращает второе наибольшее число в массиве.
-// Не используйте встроенные методы сортировки.
-function filterByAge(arr, ageLimit) {
-    return arr.filter((user) => user.age > ageLimit);
-  }
-  
-  const users = [
-    { name: "John", age: 25 },
-    { name: "Jane", age: 30 },
-    { name: "Jim", age: 20 },
-  ];
-  
-  console.log(filterByAge(users, 21));
-  // [{ name: "John", age: 25 }, { name: "Jane", age: 30 }]
-/*Уровень 7: Задачи с повышенной сложностью
-Задача 1: Фильтрация объектов в массиве по условию
-Описание: Напишите функцию, которая принимает массив объектов и возвращает только 
-те объекты, в которых значение заданного ключа больше указанного числа.*/
+/*Задача 2: Найти ключ по значению в объекте
+Описание: Напишите функцию, которая принимает объект и значение, и возвращает первый ключ, 
+который соответствует этому значению. Если ключ не найден, функция должна вернуть сообщение "Значение не найдено".*/
 
-function filterByAge(arr, ageLimit) {
-    return arr.filter(user => user.age > ageLimit);
-  }
-  
-  const users = [
-    { name: "John", age: 25 },
-    { name: "Jane", age: 30 },
-    { name: "Jim", age: 20 }
-  ];
-  
-  console.log(filterByAge(users, 21));
-  // [{ name: "John", age: 25 }, { name: "Jane", age: 30 }]
+const user = { name: 'John', age: 25, city: 'New York' };
+
+function findKeyByValue(obj, value){
+for( kl in obj ){
+  if(value == obj[kl]) return kl 
+}// 'John' == 'John'         name
+}
+
+console.log(findKeyByValue(user, 'New York'));
