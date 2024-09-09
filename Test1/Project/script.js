5f684a51a7b1a223491257f90f04d8a34ba6b907
Oleh Belenkuy <I3eLuy@gmail.com>
2024-09-09 18:10:50 +0300

    sort()

--- Test1/Project/script.js
+++ Test1/Project/script.js
@@ -1,12 +1,16 @@
-function filterByAge(arr, ageLimit) {
-  return arr.filter((user) => user.age > ageLimit);
+function sortByKeys(arr, key1, key2) {
+  return arr.slice().sort((a, b) => {
+    if (a[key1] === b[key1]) {
+      return a[key2] > b[key2] ? 1 : -1;
+    }
+    return a[key1] > b[key1] ? 1 : -1;
+  });
 }
 
 const users = [
-  { name: "John", age: 25 },
-  { name: "Jane", age: 30 },
-  { name: "Jim", age: 20 },
+  { name: "John", age: 25, city: "N York" },
+  { name: "Jane", age: 30, city: "London" },
+  { name: "Jim", age: 25, city: "Ap" },
 ];
 
-console.log(filterByAge(users, 21));
-// [{ name: "John", age: 25 }, { name: "Jane", age: 30 }]
+console.log(sortByKeys(users, "age", "city"));
