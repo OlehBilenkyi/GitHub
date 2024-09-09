c109bef3b821fca26a3929efe65132ca46816e45
Oleh Belenkuy <I3eLuy@gmail.com>
2024-09-09 18:15:38 +0300

    slice().sort() (arr,key1,key2)

--- Test1/Project/script.js
+++ Test1/Project/script.js
@@ -8,9 +8,15 @@ function sortByKeys(arr, key1, key2) {
 }
 
 const users = [
-  { name: "John", age: 25, city: "N York" },
-  { name: "Jane", age: 30, city: "London" },
+  { name: "John", age: 25, city: "Nk" },
+  { name: "Jane", age: 30, city: "Re" },
   { name: "Jim", age: 25, city: "Ap" },
+  { name: "John", age: 25, city: "XVB" },
+  { name: "Jane", age: 30, city: "Lo" },
+  { name: "Jim", age: 25, city: "WAp" },
+  { name: "John", age: 25, city: "TGF" },
+  { name: "Jane", age: 30, city: "Gndon" },
+  { name: "Jim", age: 25, city: "DFD" },
 ];
 
 console.log(sortByKeys(users, "age", "city"));
