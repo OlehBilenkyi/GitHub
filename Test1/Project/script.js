5ed33c166635a0ad7da1dba41d219230494ec6a6
Oleh Belenkuy <I3eLuy@gmail.com>
2024-09-09 18:24:54 +0300

    for(key in obj )

--- Test1/Project/script.js
+++ Test1/Project/script.js
@@ -1,22 +1,17 @@
-function sortByKeys(arr, key1, key2) {
-  return arr.slice().sort((a, b) => {
-    if (a[key1] === b[key1]) {
-      return a[key2] > b[key2] ? 1 : -1;
-    }
-    return a[key1] > b[key1] ? 1 : -1;
-  });
+function hasKey(obj,stringKey) {
+    for (key in obj){
+      if(key == stringKey){
+        return true 
+      }
+    }return false
 }
 
-const users = [
-  { name: "John", age: 25, city: "Nk" },
-  { name: "Jane", age: 30, city: "Re" },
-  { name: "Jim", age: 25, city: "Ap" },
-  { name: "John", age: 25, city: "XVB" },
-  { name: "Jane", age: 30, city: "Lo" },
-  { name: "Jim", age: 25, city: "WAp" },
-  { name: "John", age: 25, city: "TGF" },
-  { name: "Jane", age: 30, city: "Gndon" },
-  { name: "Jim", age: 25, city: "DFD" },
-];
+const obj = { 
+name: 'John',
+age: 25,
+city: 'New York',
+name1: 'John',
+color: 'New York'
+};
 
-console.log(sortByKeys(users, "age", "city"));
+console.log(hasKey(obj, 'color'));
