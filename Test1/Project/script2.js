3bc0e6e99f20278f8f750503164985e62911c8d8
Serhiy7 <ssukalo79@gmail.com>
2024-09-15 18:53:53 +0200

    123

--- Test1/Project/script2.js
+++ Test1/Project/script2.js
@@ -1,29 +1,26 @@
-export default class XUY {
-  constructor() {
-    this.items = [];
-  }
+/**Объединение объектов с суммированием значений: Реализуй функцию mergeObjects(arr), которая принимает массив объектов с одинаковыми ключами,
+ *  а значениями могут быть числа. Необходимо объединить объекты так, чтобы значения числовых полей суммировались. */
+const objects = [
+  { apples: 5, oranges: 10 },
+  { apples: 3, oranges: 8 },
+  { apples: 7, bananas: 5 }
+];
 
-  push(item) {
-    this.items.push(item);
-    return this.items.length;
+function mergeObjects(arr) {
+  let newObj = {};
+  for(let obj of arr ){  
+      for(let key in obj ){
+          if(newObj[key]){   //      Если ключ уже существует в новом объекте (newObj[key]), это означает, что он уже встречался ранее, и его значение нужно обновить, прибавив текущее значение.
+              newObj[key] += obj[key];
+          } else {
+              newObj[key] = obj[key]
+          }
+      }
   }
-
-  pop() {
-    return this.items.pop();
-  }
-
-  isEmpty() {
-    return this.items.length === 0;
-  }
-
-  peek() {
-    return this.items[this.items.length - 1];
-  }
-
-  length() {
-    return this.items.length;
-  }
+return newObj
 }
-const zalupa = new XUY();
 
-zalupa.length
+
+console.log(mergeObjects(objects));
+
+// { apples: 15, oranges: 18, bananas: 5 }
